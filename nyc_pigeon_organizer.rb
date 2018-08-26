0a5f2a7e0c0c8e1253ed54e4e4180a5a6e742729
DeCode2018 <decode2018@gmail.com>
2018-08-26 13:42:09 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+
+name_arr = []
+att = []
+
+    data.each do |attributes, value|
+      att << attributes
+      value.each do |k,name|
+        name.each do |n|
+          name_arr << n
+        end
+      end
+    end
+
+pigeon_list ={}
+
+name_arr.each do |n|
+  pigeon_list[n] = {}
+att.each do |x|
+  pigeon_list[n][x] = []
+end
+end
+
+pigeon_list.each do |name, hash|
+  hash.each do |k,a|
+    data[k].each do |key, array|
+      array.each do |n|
+        if n == name
+          pigeon_list[name][k] << key.to_s
+        end
+      end
+    end
+  end
+end
+
+pigeon_list
 end
